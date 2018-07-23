5a18a8a004b066674fd92a2d7af3aa59ce9ab913
chunshun <woaiboss@126.com>
2018-07-23 09:58:56 +0800

    queue

--- interview/parts/stack_queue/balanced_general.py
+++ interview/parts/stack_queue/balanced_general.py
@@ -3,7 +3,33 @@ from init import Stack
 string='([{}])'
 left ='([{'
 right=')]}'
+# index=0
+# parent='()'
+# data=string.split()
 index=0
+balanced=True
+print(list(string))
 while index<len(string):
-
+    if stack.is_empty() and string[index]==')':
+        balanced=False
+        break
+    else:
+        x=string[index]
+        if x in parent:
+            stack.push(x) 
+            print(stack._data)
+            if x==')':
+                stack.pop()
+                y=stack.pop()
+                print(stack._data)
+                if y==')':
+                    balanced=False
+                    break
+            
+    print(index,x)   
     index+=1
+        # stack.push(string[index]) 
+if not stack.is_empty():
+    balanced=False
+    
+print(balanced)
--- interview/parts/stack_queue/note.md
+++ interview/parts/stack_queue/note.md
@@ -1,3 +1,7 @@
 1. stack 
 - simple balance parentheses
-- 
+- conversion of infix expression to prefix and postfix
+- general infix-to-postfix conversion
+- postfix evaluation
+
+2. Queue
