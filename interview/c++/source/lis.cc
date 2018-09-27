9903eab03ccb1076bfb51cc88ca395120646be55
chunshun <woaiboss@126.com>
2018-09-27 21:29:37 +0800

    c++

--- interview/c++/source/lis.cc
+++ interview/c++/source/lis.cc
@@ -1,31 +1,39 @@
-/*to find the longest subsequence of a given sequence in increasing order
-* dp[i]: the lis of A[i] ended with index i
-* dp[i]=max(dp[j]+1 , 1) where j<i and A[j]<A[i]
+/* the overlapping problem is:
+*LIS is a special case of edit distance, LIS:to find the longest increasing subsequence(similar to the MSIS(to find the i-th increasing array))
+* L[i]:bottom-up approach
+* dp[i] denotes that the longest increasing  subsequence ended with the A[i]
+* then dp[i]=max(1,dp[j]+1),where j=1,2,3,...,n-1  and A[j]<A[i]
+* if the item is larger than the previous element in the LIS,than add the item in the set
+* else exclude the current item
 */
 #include <iostream>
+#include <string.h>
 using namespace std;
-int lis_index(int array[],int n){
-    int items[n+1][n+1];
-    if (n==1){
-        return 1;
-    }
-    else{
-        // int count =0;
-        for(int i = 0; i < n; i++)
+void LIS(int array[], int n)
+{
+    // int n = sizeof(array) / sizeof(*array);
+    // std::cout<
+    std::cout << n << endl;
+    int record[n];
+    memset(record, 0, sizeof(record));
+    for (int i = 0; i < n; i++)
+    {
+        int item[i];
+
+        for (int j = 1; j < i; j++)
         {
-            
-            if (array[i]<array[n])
-                // count++;
-                re
+            cout << array[i] << endl;
+            if(array[j-1]<array[j] && array[j]<array[i]){
+                cout<<array[j]<<"\t";
+            }
         }
-        return n;
     }
-    
 }
+
 int main(int argc, char const *argv[])
 {
-    int array[]={12,3,4,4,5,6};
-    cout<<sizeof(array)<<"-----------------\t"<<sizeof(array[0])<<endl;
-
+    int array[5] = {
+        1, 2, 3, 4, 5};
+    LIS(array, 5);
     return 0;
 }
